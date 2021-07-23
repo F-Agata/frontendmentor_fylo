7719f3e181466b0091bd8a09f2384e585b5b8f9a
F-Agata <hello@agataf.pl>
2021-07-23 11:26:35 +0200

    initial commit

---
+++ src/mainComponents/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import "../scss/Navigation.scss"
+
+
+function Navigation() {
+    return (
+        <div className="App">
+          jestem menu
+
+
+        </div>
+    );
+}
+
+export default Navigation;
