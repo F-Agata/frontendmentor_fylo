dc84906cd871ca14aa27bcaaaa2f9ba28662ebcb
F-Agata <hello@agataf.pl>
2021-07-26 11:08:16 +0200

    add scss to Header

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ function App() {
     <div className="App">
         <Navigation/>
         <Header/>
+        <MainBenefits/>
+
 
 
     </div>
--- src/mainComponents/Headre.js
+++ src/mainComponents/Headre.js
@@ -13,6 +13,12 @@ function Header() {
                 <img className={"header__illustration"} src={headerIllustration} alt={"headerIllustration"}/>
             </div>
 
+        <div className={"header__text"}>
+            <h1 className={"header__title"}>All your files in one secure location, accessible anywhere.</h1>
+            <p className={"header__paragraph"}>Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
+            <button className={"header__btnGetStarted"}>Get Started</button>
+        </div>
+
 
         </div>
     );
